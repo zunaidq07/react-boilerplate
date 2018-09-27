5ec60c9ba80a79f44e3bca4f6915398677052a61
Quazi Zunaid <quazunai@publicisgroupe.net>
2018-09-27 18:32:31 +0530

    boilerplate

---
+++ webpack.config.development.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  // Webpack configuration goes here
+  mode: 'development',
+  entry: {
+    vendor: ['semantic-ui-react'],
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].[hash].js'
+  },
+  optimization: {
+   splitChunks: {
+     cacheGroups: {
+       vendor: {
+         chunks: 'initial',
+         test: 'vendor',
+         name: 'vendor',
+         enforce: true
+       }
+     }
+   }
+ },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+
+      // First Rule
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      // Second Rule
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+      // favicon: 'public/favicon.ico'
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  }
+};
---
+++ webpack.config.production.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  // Webpack configuration goes here
+  mode: 'development',
+  entry: {
+    vendor: ['semantic-ui-react'],
+    app: './src/index.js'
+  },
+  output: {
+    filename: 'static/[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  optimization: {
+   splitChunks: {
+     cacheGroups: {
+       vendor: {
+         chunks: 'initial',
+         test: 'vendor',
+         name: 'vendor',
+         enforce: true
+       }
+     }
+   }
+ },
+  devtool: 'source-map',
+  module: {
+    rules: [
+
+      // First Rule
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      // Second Rule
+      {
+        test: /\.css$/,
+        // We configure 'Extract Text Plugin'
+        use: ExtractTextPlugin.extract({
+          // loader that should be used when the
+          // CSS is not extracted
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                // Allows to configure how many loaders
+                // before css-loader should be applied
+                // to @import(ed) resources
+                importLoaders: 1,
+                camelCase: true,
+                // Create source maps for CSS files
+                sourceMap: true
+              }
+            },
+            {
+              // PostCSS will run before css-loader and will
+              // minify and autoprefix our CSS rules.
+              loader: 'postcss-loader',
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+      // favicon: 'public/favicon.ico'
+    }),
+    // Create the stylesheet under 'styles' directory
+    new ExtractTextPlugin({
+      filename: 'styles/styles.[hash].css',
+      allChunks: true
+    })
+  ]
+};
